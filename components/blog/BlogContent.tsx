c23c92b4d1be09fb441aa687164532a534975c34
Gia Kinh <giakinh2000@gmail.com>
2021-12-13 05:09:28 +0700

    fix bug

--- components/blog/BlogContent.tsx
+++ components/blog/BlogContent.tsx
@@ -48,7 +48,6 @@ export default function BlogContent({ data }: IBlogContentProps) {
         <Box sx={{ "& code": { fontSize: "20px" } }}>
           <ReactMarkdown
             rehypePlugins={[rehypeRaw, remarkGfm]}
-            escapeHtml={false}
             components={{
               code({ node, inline, className, children, ...props }: any) {
                 const match = /language-(\w+)/.exec(className || "");
