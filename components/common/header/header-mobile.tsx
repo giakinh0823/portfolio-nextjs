94a0caa71b4cc09d3a2514fb2e906b1e91378031
Gia Kinh <giakinh2000@gmail.com>
2021-12-14 04:28:58 +0700

    fix menu

--- components/common/header/header-mobile.tsx
+++ components/common/header/header-mobile.tsx
@@ -1,8 +1,81 @@
-import { Box } from '@mui/system'
-import * as React from 'react'
+import {
+  Button,
+  Container,
+  Drawer,
+  Link as MuiLink,
+  Slide,
+  Stack,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import * as React from "react";
+import { useAppSelector } from "../../../app/hooks";
+import { selectMode } from "../../../redux/mode/modeSlice";
+import UseSwitchesCustom from "../SwitchDarkMode";
+import { ROUTE_LIST } from "./routes";
+import { IconButton } from '@mui/material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 export interface HeaderMobileProps {}
 
 export function HeaderMobile(props: HeaderMobileProps) {
-	return <Box display={{ xs: 'block', md: 'none' }}>Header Mobile</Box>
+  const router = useRouter();
+  const mode = useAppSelector(selectMode);
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <Box display={{ xs: "block", md: "none" }}>
+      <Box py={2} sx={{}}>
+        <Container>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Link href={"/"} passHref>
+                <MuiLink>
+                  <Stack direction="row" alignItems="center">
+                    <Box component="span" sx={{ fontSize: "1.2rem" }}>
+                      👋
+                    </Box>
+                    <Box sx={{ ml: 1, fontWeight: "bold", fontSize: "1.2rem" }}>
+                      Kính G.
+                    </Box>
+                  </Stack>
+                </MuiLink>
+              </Link>
+            </Box>
+            <Stack direction="row">
+			  <IconButton  onClick={() => setIsOpen(true)}><MenuOutlinedIcon sx={{fontSize: "30px"}}/></IconButton>
+              <Stack alignItems="center" ml={2}>
+                <UseSwitchesCustom />
+              </Stack>
+            </Stack>
+          </Stack>
+        </Container>
+      </Box>
+      <Drawer anchor="left" open={isOpen} onClose={() => setIsOpen(false)}>
+        <Box sx={{ minWidth: "250px", padding: "30px 0" }}>
+          <Stack direction="column" alignItems="center">
+            {ROUTE_LIST.map((route, index) => (
+              <Box key={index} sx={{padding: "15px 0"}}>
+                <Link key={route.path} href={route.path} passHref>
+                  <MuiLink
+                    sx={{ fontWeight: "bold", fontSize: "1.1rem" }}
+                    className={clsx({
+                      active: router.pathname === route.path,
+                    })}
+                  >
+                    {route.label}
+                  </MuiLink>
+                </Link>
+              </Box>
+            ))}
+          </Stack>
+        </Box>
+      </Drawer>
+    </Box>
+  );
 }
