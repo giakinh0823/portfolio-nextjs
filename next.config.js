4b19c9e9f814d52a3785842bae5e4cb88a328266
Gia Kinh <giakinh2000@gmail.com>
2021-12-13 13:33:37 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   reactStrictMode: true,
   optimizeFonts: false,
   images: {
-    domains: ['localhost','hagiakinh-xyz.herokuapp.com','firebasestorage.googleapis.com'],
+    domains: ['localhost','hagiakinh-xyz.herokuapp.com','cms.hagiakinh.xyz','firebasestorage.googleapis.com'],
   },
 }
