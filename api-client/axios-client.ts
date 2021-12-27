15ee4d1ac5fd3e03256788375f6ebccd7bd20a60
Gia Kinh <giakinh2000@gmail.com>
2021-12-27 23:03:27 +0700

    fix api

--- api-client/axios-client.ts
+++ api-client/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-	baseURL: 'https://api.hagiakinh.me/api',
+	baseURL: 'https://api.hagiakinh.me/api/',
 	headers: {
 		'Content-Type': 'application/json',
 	},
